0c535e147b58424da53c89b2cbfe42c83074c444
nadinsylaa <asepmlnaaa@gmail.com>
2024-02-16 18:28:48 +0000

    sepolicy: Adjust sepolicy paths for NavaDroid
    Signed-off-by: nadinsylaa <asepmlnaaa@gmail.com>

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -1,12 +1,12 @@
 #
 # This policy configuration will be used by atv products that
-# inherit from Lineage
+# inherit from NavaDroid
 #
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/atv/vendor
+    device/nava/sepolicy/atv/vendor
 endif
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/private
+   device/nava/sepolicy/atv/private
--- exynos/sepolicy.mk
+++ exynos/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all exynos products
-# that inherit from Lineage
+# that inherit from NavaDroid
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -10,14 +10,14 @@ endif
 endif
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/exynos/private
+    device/nava/sepolicy/exynos/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/exynos/dynamic \
-    device/lineage/sepolicy/exynos/system
+    device/nava/sepolicy/exynos/dynamic \
+    device/nava/sepolicy/exynos/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/exynos/dynamic \
-    device/lineage/sepolicy/exynos/vendor
+    device/nava/sepolicy/exynos/dynamic \
+    device/nava/sepolicy/exynos/vendor
 endif
